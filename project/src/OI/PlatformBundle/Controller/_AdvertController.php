4ef9f8356784e4b55b8bf6caf11961ceae143969
cdufour <opusidea@gmail.com>
2016-11-29 11:46:27 +0100

    commit 4

--- project/src/OI/PlatformBundle/Controller/_AdvertController.php
+++ project/src/OI/PlatformBundle/Controller/_AdvertController.php
@@ -10,7 +10,25 @@ class AdvertController extends Controller
 {
     public function indexAction(Request $request)
     {
+      /*
+      $session = $request->getSession(); // on récupère la session
+      $session->set('user_name', 'Paolo'); // on écrit dans la session
+      $userName = $session->get('user_name'); // on récupère une variable de session
 
+      $friends = ["Mourad", "Nabil", "Yassine"];
+      $infos = array(
+        "country" => "Maroc",
+        "town" => "Casablanca",
+        "retired" => false
+      );
+
+      return $this->render('OIPlatformBundle:Advert:index.html.twig', array(
+        'name' => $userName,
+        'age' => 15,
+        'friends' => $friends,
+        'infos' => $infos
+      ));
+      */
       $listAdverts = array(
         array(
           'id' => 1,
@@ -21,7 +39,7 @@ class AdvertController extends Controller
         array(
           'id' => 2,
           'title' => 'Offre de stage à Montpellier',
-          'author' => 'Burdy',
+          'author' => 'Chris',
           'content' => "Dans le cadre d'un partenariat avec le Juventus FC, nous recherchons un développeur à la fois à l'aise avec Angular et habile balle au pied",
           'date' => new \Datetime()),
         array(
@@ -38,17 +56,32 @@ class AdvertController extends Controller
     }
 
     public function viewAction($id, Request $request) {
-      // TODO: requête en db
-      $advert = array(
-        'id' => 1,
-        'title' => 'Urgent: recherche développeur Angular',
-        'author' => 'Chris',
-        'content' => "Dans le cadre d'un partenariat avec le Juventus FC, nous recherchons un développeur à la fois à l'aise avec Angular et habile balle au pied",
-        'date' => new \Datetime());
+      //return $this->render('OIPlatformBundle:Advert:view.html.twig');
 
-      return $this->render('OIPlatformBundle:Advert:view.html.twig', array(
-        'advert' => $advert
-      ));
+      // permet de récupérer la clé tag dans l'url (?tag=)
+      $tag = $request->query->get('tag');
+
+      // generateUrl() => retourne l'url par rapport au nom de route fourni en entrée
+      //$url = $this->generateUrl('oi_platform_home');
+
+      /*
+      return new Response(
+        "<p>Vous souhaitez afficher l'annonce: ".$id."
+         avec le tag : ".$tag."</p>
+        <br><a href=".$url.">Accueil</a>"
+      );
+      */
+
+      // création d'une reponse, sans contenu à cet instant
+      $response = new Response();
+
+      // insertion de contenu dans la réponse
+      $response->setContent("<html><body>Tout va bien</body></html>");
+
+      // on définit le statut à erreur 404
+      $response->setStatusCode(Response::HTTP_NOT_FOUND);
+
+      return $response;
     }
 
     public function viewSlugAction($slug, $year, $format) {
